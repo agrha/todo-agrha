cd1146be2cc5e376b05a1e068e5b74ef5acd2eda
agrha <agrhakartz@gmail.com>
2018-03-19 16:49:16 +0700

    todo controller

---
+++ server/controllers/todos.js
@@ -0,0 +1,93 @@
+const Todo = require('../models/Todos')
+
+class TodoController {
+  static createTodo(req,res){
+    let todo = {
+        name: req.body.name,
+        userId :req.headers.userId
+    }
+    Todo.create(todo)
+    .then(data=>{
+      res.status(200).send({message:'todo created',data})
+    })
+    .catch(err=>{
+      res.status(500).send({
+        message:'something went wrong',
+        err
+      })
+    })
+  }
+
+  static findTodo(req,res){
+    Todo.find({
+      userId:req.headers.userId
+    })
+    .populate('userId')
+    .then(data=>{
+      res.status(200).send({message:'your todolist',data})
+    })
+    .catch(err=>{
+      res.status(500).send({
+        message:'something went wrong',
+        err
+      })
+    })
+  }
+
+  static deleteTodo(req,res){
+    Todo.remove({_id:req.params.id})
+    .then(data=>{
+      res.status(200).send({
+        message:'todo has been deleted'
+      })
+    })
+    .catch(err=>{
+      res.status(500).send({
+        message:'something went wrong',
+        err
+      })
+    })  
+  }
+
+  static editTodo(req,res){
+    let todo = {
+      name:req.body.name,
+    }
+    Todo.findOneAndUpdate({_id:req.params.id},todo)
+    .then(data=>{
+      res.status(200).send({
+        message:'data updated succesfully',
+        data
+      })
+    })
+    .catch(err=>{
+      res.status(500).send({
+        message:'something went wrong',
+        err
+      })
+    })
+  }
+
+  static completeTodo(req,res){
+    Todo.findById(req.params.id)
+    .then(data=>{
+      data.status = !data.status
+      data.save()
+      .then(result=>{
+        res.status(200).send({
+          message:'todo completed',
+          data
+        })
+      })
+      .catch(err=>{
+        res.status(500).send({
+          message:'something went wrong',
+          err
+        })
+      })
+    })
+  }
+
+}
+
+module.exports = TodoController
